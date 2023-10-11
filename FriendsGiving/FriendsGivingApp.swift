37fa70b80b50c028d06078364c1a14165988cbd3
AntonyBil <beletskiy.ant@gmail.com>
2023-10-11 21:10:08 +0300

    Initial commit

---
+++ FriendsGiving/FriendsGivingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsGivingApp.swift
+//  FriendsGiving
+//
+//  Created by apple on 11.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct FriendsGivingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
